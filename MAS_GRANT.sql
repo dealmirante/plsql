b859dd319b564b72214709d03fba9e6ca793df81
dealmirante <dealmirante@gmail.com>
2021-05-30 21:20:40 -0300

    sql propios

---
+++ MAS_GRANT.sql
@@ -0,0 +1,3 @@
+GRANT SELECT,insert,update,delete ON porciento_local_rubro TO role_ESTADIStico
+
+role_consulta
---
+++ acum_mes.sql
@@ -0,0 +1,2 @@
+exec ejecutar_acum_mes_depn(to_date('20-11-2006','dd-mm-yyyy'),'AEP','AEP','   ', 22);
+commit ; 
